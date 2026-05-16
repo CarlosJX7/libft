4a2d89ac7e86d2cf75507df8e66146fd4482c789
CarlosJX7 <carlos.jx7@gmail.com>
2026-05-16 18:55:56 +0200

    Add memory functions

--- src/memory/ft_memcpy.c
+++ src/memory/ft_memcpy.c
@@ -17,4 +17,25 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	return (dst);
 }
 
-//implementar memmove para el solapamiento
+// #include <string.h>
+// #include <stdio.h>
+// int main(void)
+// {
+// 	{
+// 		char str1[] = "AAAA";
+// 		char str2[] = "BBBB";
+// 		char *dest;
+// 		size_t n = 2;
+// 		dest = ft_memcpy(str2, str1, n);
+// 		printf("Caso 1 (sin solapamiento): %s\n", dest);
+// 	}
+// 	{// memcpy no gestiona el solapamiento
+// 		char buffer[] = "HOLA1234";
+// 		size_t n = 4;
+
+// 		printf("Buffer antes: %s\n", buffer);
+// 		ft_memcpy(buffer + 1, buffer, n);
+// 		printf("Caso 2 (con solapamiento): %s\n", buffer);
+// 	}
+// 	return 0;
+// }
--- src/memory/ft_memmove.c
+++ src/memory/ft_memmove.c
@@ -2,5 +2,51 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t n)
 {
-	return (NULL);
+	char	*tmp;
+	char	*srce;
+	char	*dest;
+	size_t	i;
+
+	tmp = malloc(sizeof(char) * n);
+	if (!tmp)
+		return (NULL);
+	srce = (char *)src;
+	dest = (char *)dst;
+	i = 0;
+	while (i < n)
+	{
+		tmp[i] = srce[i];
+		i++;
+	}
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = tmp[i];
+		i++;
+	}
+	free(tmp);
+	return ((void *)dest);
 }
+
+// #include <string.h>
+// #include <stdio.h>
+// int main(void)
+// {
+// 	{
+// 		char str1[] = "AAAA";
+// 		char str2[] = "BBBB";
+// 		char *dest;
+// 		size_t n = 2;
+// 		dest = ft_memmove(str2, str1, n);
+// 		printf("Caso 1 (sin solapamiento): %s\n", dest);
+// 	}
+// 	{// memmove gestiona el solapamiento guardando el buffer > tmp
+// 		char buffer[] = "HOLA1234";
+// 		size_t n = 4;
+
+// 		printf("Buffer antes: %s\n", buffer);
+// 		memmove(buffer + 1, buffer, n);
+// 		printf("Caso 2 (con solapamiento): %s\n", buffer);
+// 	}
+// 	return 0;
+// }
