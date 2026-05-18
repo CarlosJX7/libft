0ac684a88d1e57ff8b19ede65b4c9ace479d6981
CarlosJX7 <carlos.jx7@gmail.com>
2026-05-19 00:42:23 +0200

    Add libft changes

--- src/string/ft_strdup.c
+++ src/string/ft_strdup.c
@@ -9,3 +9,36 @@
 /*   Updated: 2026/05/18 19:43:27 by carlinaq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../../libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	size_t	len;
+	size_t	i;
+	char	*new_str;
+
+	len = ft_strlen(s) + 1;
+	new_str = malloc(sizeof(char) * len);
+	if (!new_str)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		new_str[i] = s[i];
+		i++;
+	}
+	new_str[i] = '\0';
+	return (new_str);
+}
+
+// #include <stdio.h>
+// int main(void)
+// {
+// 	char *str = "Hello World";
+// 	char *new_str;
+// 	new_str = ft_strdup(str);
+// 	printf(">%s< -> %p\n", str, &str);
+// 	printf(">%s< -> %p\n", new_str, &new_str);
+// 	return 0;
+// }
--- src/string/ft_substr.c
+++ src/string/ft_substr.c
@@ -9,3 +9,50 @@
 /*   Updated: 2026/05/18 19:44:43 by carlinaq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../../libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char *new_str;
+	size_t	i;
+	size_t len_s;
+
+	len_s = ft_strlen(s) - start - 1;
+	i = 0;
+	if (len_s > 0)
+	{
+		new_str = malloc(sizeof(char) * (len + 1));
+		if (!new_str)
+			return (NULL);
+	}else
+	{
+		new_str = malloc(sizeof(char) * 1);
+		if (!new_str);
+			return(NULL);
+		new_str[0] = '\0';
+			return (new_str);
+	}
+	while (s[i] && i < len)
+	{
+		new_str[i] = s[start + i];
+		i++;
+	}
+	new_str[i] = '\0';
+	return (new_str);
+}
+
+// #include <stdio.h>
+// int main(void)
+// {
+// 	char *str = "123456789";
+// 	char *new_str;
+
+// 	new_str = ft_substr(str, 3, 109);
+// 	printf("%s\n", new_str);
+// 	free(new_str);
+// 	new_str = ft_substr(str, 3, 2);
+// 	printf("%s\n", new_str);
+// 	free(new_str);
+// 	return 0;
+// }
