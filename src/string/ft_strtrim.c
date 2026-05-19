86b0fcb54eb593055a34248eb2bc78b5be23318d
CarlosJX7 <carlos.jx7@gmail.com>
2026-05-19 15:26:20 +0200

    Add string update

--- src/string/ft_strtrim.c
+++ src/string/ft_strtrim.c
@@ -12,7 +12,29 @@
 
 #include "../../libft.h"
 
+#include <stdio.h>
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	
+	size_t	i;
+	//char *str;
+	i = 0;
+	while (s1[i])
+	{
+		if (ft_strnstr(&s1[i], set, ft_strlen(set)))
+		{
+			i += ft_strlen(set);
+		}
+		printf("%c\n", s1[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+int main(void)
+{
+	char *s1 = "aaabbbaaaa";
+	char *s2 = "bbb";
+	ft_strtrim(s1, s2);
+	return 0;
 }
