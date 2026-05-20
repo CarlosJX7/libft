eb10f3532d824460803aa5ea8f771e6c5ce8c0b0
CarlosJX7 <carlos.jx7@gmail.com>
2026-05-20 19:09:41 +0200

    Add string norm

--- src/memory/ft_memcpy.c
+++ src/memory/ft_memcpy.c
@@ -15,15 +15,13 @@
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t			i;
-	unsigned char	*dest;
 	unsigned char	*srce;
 
-	dest = (unsigned char *)dst;
 	srce = (unsigned char *)src;
 	i = 0;
 	while (i < n)
 	{
-		dest[i] = srce[i];
+		((unsigned char *)dst)[i] = srce[i];
 		i++;
 	}
 	return (dst);
