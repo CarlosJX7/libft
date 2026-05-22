d653db5b70d61c2afa8ad7bf200e7448f455f9a1
Carlos <98550389+CarlosJX7@users.noreply.github.com>
2026-05-22 10:52:51 +0000

    Add memcpy changes

--- src/memory/ft_memcpy.c
+++ src/memory/ft_memcpy.c
@@ -16,12 +16,14 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t			i;
 	unsigned char	*srce;
+	unsigned char	*dest;
 
 	srce = (unsigned char *)src;
+	dest = (unsigned char *)dst;
 	i = 0;
 	while (i < n)
 	{
-		((unsigned char *)dst)[i] = srce[i];
+		dest[i] = srce[i];
 		i++;
 	}
 	return (dst);
@@ -42,7 +44,6 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 // 	{// memcpy no gestiona el solapamiento
 // 		char buffer[] = "HOLA1234";
 // 		size_t n = 4;
-
 // 		printf("Buffer antes: %s\n", buffer);
 // 		ft_memcpy(buffer + 1, buffer, n);
 // 		printf("Caso 2 (con solapamiento): %s\n", buffer);
