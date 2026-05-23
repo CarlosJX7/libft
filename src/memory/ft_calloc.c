339d33acf1d0cb2e19c0dffa38519b95a260d704
CarlosJX7 <carlos.jx7@gmail.com>
2026-05-23 17:18:23 +0200

    Add .h & memory func updates

--- src/memory/ft_calloc.c
+++ src/memory/ft_calloc.c
@@ -17,56 +17,56 @@ void	*ft_calloc(size_t count, size_t size)
 	unsigned char	*mem;
 	size_t			total;
 
-	if (count != 0 && size > 18446744073709551615UL / count)
+	if (count != 0 && size > SIZE_MAX / count)
 		return (NULL);
 	total = count * size;
 	mem = malloc(total);
 	if (!mem)
 		return (NULL);
-	while (total-- > 0)
-		mem[total] = 0;
+	while (total > 0)
+	{
+		mem[total - 1] = 0;
+		total--;
+	}
 	return ((void *)mem);
 }
-
-/*
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-#include <stdint.h>
-int main(void)
-{
-	char *mem1;
-	char *mem2;
-	size_t count;
-	size_t size;
-	int i;
-	count = 16  * 1000;
-	size = 16;
-	printf("=== ft_calloc ===\n");
-	mem1 = ft_calloc(count, size);
-	if (!mem1)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	i = 0;
-	while (i < 4)
-	{
-		printf("%d\n", mem1[i]);
-		i++;
-	}
-	free(mem1);
-	printf("\n=== calloc original ===\n");
-	mem2 = calloc(count , size);
-	if (!mem2)
-		return (1);
-	i = 0;
-	while (i < 4)
-	{
-		printf("%d\n", mem2[i]);
-		i++;
-	}
-	free(mem2);
-	return (0);
-}
-*/
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <limits.h>
+// #include <stdint.h>
+// int main(void)
+// {
+// 	char *mem1;
+// 	char *mem2;
+// 	size_t count;
+// 	size_t size;
+// 	int i;
+// 	count = (SIZE_MAX + 1) / 10;
+// 	size = 10;
+// 	printf("=== ft_calloc ===\n");
+// 	mem1 = ft_calloc(count, size);
+// 	if (!mem1)
+// 	{
+// 		printf("Error\n");
+// 		return (1);
+// 	}
+// 	i = 0;
+// 	while (i < 4)
+// 	{
+// 		printf("%d\n", mem1[i]);
+// 		i++;
+// 	}
+// 	free(mem1);
+// 	printf("\n=== calloc original ===\n");
+// 	mem2 = calloc(count , size);
+// 	if (!mem2)
+// 		return (1);
+// 	i = 0;
+// 	while (i < 4)
+// 	{
+// 		printf("%d\n", mem2[i]);
+// 		i++;
+// 	}
+// 	free(mem2);
+// 	return (0);
+// }
--- src/memory/ft_memmove.c
+++ src/memory/ft_memmove.c
@@ -26,13 +26,13 @@ void	ft_move(unsigned char *dest, unsigned char *src, size_t n)
 
 void	ft_move_back(unsigned char *dest, unsigned char *src, size_t n)
 {
-	int len;
+	int	len;
 
-	len = (int)n;
+	len = (int)n - 1;
 	while (len >= 0)
 	{
 		dest[len] = src[len];
-		n--;
+		len--;
 	}
 }
 
@@ -61,14 +61,29 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 // 		char *dest;
 // 		size_t n = 2;
 // 		dest = ft_memmove(str2, str1, n);
-// 		printf("Caso 1 (sin solapamiento): %s\n", dest);
+// 		printf("Caso 1(libft): %s\n", dest);
+// 	}
+// 	{
+// 		char str1[] = "AAAA";
+// 		char str2[] = "BBBB";
+// 		char *dest;
+// 		size_t n = 2;
+// 		dest = memmove(str2, str1, n);
+// 		printf("Caso 1(string): %s\n", dest);
+// 	}
+// 	{
+// 		char buffer[] = "HOLA1234";
+// 		size_t n = 4;
+// 		printf("Buffer antes: >%s<\n", buffer);
+// 		memmove(buffer + 1, buffer, n);
+// 		printf("Caso 2 (intentando solapamiento): >%s<\n", buffer);
 // 	}
 // 	{// memmove gestiona el solapamiento guardando el buffer > tmp
 // 		char buffer[] = "HOLA1234";
 // 		size_t n = 4;
-// 		printf("Buffer antes: %s\n", buffer);
-// 		memmove(buffer + 1, buffer, n);
-// 		printf("Caso 2 (intentando solapamiento): %s\n", buffer);
+// 		printf("Buffer antes: >%s<\n", buffer);
+// 		ft_memmove(buffer + 1, buffer, n);
+// 		printf("Caso 2 (intentando solapamiento): >%s<\n", buffer);
 // 	}
 // 	return 0;
 // }
